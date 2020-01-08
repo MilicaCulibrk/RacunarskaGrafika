728cddb7526dedc8c0922aa6a6b9d14baa2f48cf
MilicaCulibrk <milica6.1.97thebest@gmail.com>
2020-01-08 19:11:20 +0100

    Bedni pokusaji

--- AssimpSample/MainWindow.xaml.cs
+++ AssimpSample/MainWindow.xaml.cs
@@ -15,14 +15,15 @@ using System.Windows.Navigation;
 using SharpGL.SceneGraph;
 using SharpGL;
 using Microsoft.Win32;
-
+using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 namespace AssimpSample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         #region Atributi
 
@@ -30,6 +31,32 @@ namespace AssimpSample
         ///	 Instanca OpenGL "sveta" - klase koja je zaduzena za iscrtavanje koriscenjem OpenGL-a.
         /// </summary>
         World m_world = null;
+
+        private int _personSize = 100;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public int PersonSize
+        {
+            get { return _personSize; }
+            set
+            {
+                _personSize = value;
+                OnPropertyChanged("PersonSize");
+                float forScaling = (float)_personSize / 100;
+
+                m_world.MMScale = forScaling;
+
+            }
+
+        }
 
         #endregion Atributi
 
@@ -84,37 +111,166 @@ namespace AssimpSample
             m_world.Resize(args.OpenGL, (int)openGLControl.ActualWidth, (int)openGLControl.ActualHeight);
         }
 
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.Key)
             {
-                case Key.F10: this.Close(); break;
-                case Key.W: m_world.RotationX -= 5.0f; break;
-                case Key.S: m_world.RotationX += 5.0f; break;
-                case Key.A: m_world.RotationY -= 5.0f; break;
-                case Key.D: m_world.RotationY += 5.0f; break;
-                case Key.Add: m_world.SceneDistance -= 700.0f; break;
-                case Key.Subtract: m_world.SceneDistance += 700.0f; break;
-                case Key.F2:
-                    OpenFileDialog opfModel = new OpenFileDialog();
-                    bool result = (bool) opfModel.ShowDialog();
-                    if (result)
+                case Key.F4: this.Close(); break;
+                 case Key.E:
+                    if (World.startAnimation == false)
                     {
+                        if (m_world.RotationX > -85.0f)
+                        {
+                            m_world.RotationX -= 5f;
+                        }
 
-                        try
+                    }
+                        break;
+                case Key.D:
+                    if (World.startAnimation == false)
+                    {
+                        if (m_world.RotationX < 85.0f)
                         {
-                            World newWorld = new World(Directory.GetParent(opfModel.FileName).ToString(), Path.GetFileName(opfModel.FileName), (int)openGLControl.Width, (int)openGLControl.Height, openGLControl.OpenGL);
-                            m_world.Dispose();
-                            m_world = newWorld;
-                            m_world.Initialize(openGLControl.OpenGL);
+                            m_world.RotationX += 5f;
                         }
-                        catch (Exception exp)
-                        {
-                            MessageBox.Show("Neuspesno kreirana instanca OpenGL sveta:\n" + exp.Message, "GRESKA", MessageBoxButton.OK );
-                        }
+
+                    }
+                        break;
+                case Key.S:
+                    if (World.startAnimation == false)
+                    {
+                        m_world.RotationY -= 5f;
+
+                    }
+                    break;
+                case Key.F:
+                    if (World.startAnimation == false)
+                    {
+                        m_world.RotationY += 5f;
+
+                    }
+                    break;
+                case Key.Subtract:
+                    if (World.startAnimation == false)
+                    {
+                        m_world.SceneDistance += 100.0f;
+
+                    }
+                    break;
+
+                case Key.Add:
+                    if (World.startAnimation == false)
+                    {
+                        m_world.SceneDistance -= 100.0f;
+
+                    }
+                    break;
+                case Key.V:
+                    if (World.startAnimation)
+                    {
+                        World.timer.Stop();
+                        World.timer = null;
+                        World.startAnimation = false;
+                    }
+                    else
+                    {
+                        World.startAnimation = true;
+                        World.timer = new System.Windows.Threading.DispatcherTimer();
+                        World.timer.Tick += new EventHandler(World.Animacija);
+                        World.timer.Interval = TimeSpan.FromMilliseconds(600);
+                        World.x = -180f;
+                        World.y = -205f;
+                        World.z = -100f;
+                        World.timer.Start();
                     }
                     break;
             }
+
+
         }
+
+        private void PersonSize_KeyUp(object sender, KeyEventArgs e)
+        {
+            int n;
+            bool number = Int32.TryParse(PersonSizeTextBox.Text, out n);
+            if (number)
+            {
+                if (PersonSize != n)
+                    PersonSize = n;
+            }
+
+        }
+
+
+        private void rotirajIgloMinus(object sender, RoutedEventArgs e)
+        {
+            if (World.startAnimation == false)
+            {
+                m_world.RotateY -= 5.0f;
+
+            }
+
+        }
+        private void rotirajIgloPlus(object sender, RoutedEventArgs e)
+        {
+            if (World.startAnimation == false)
+            {
+                m_world.RotateY += 5.0f;
+
+            }
+        }
+        private void rMinus(object sender, RoutedEventArgs e)
+        {
+            if (m_world.rAmbient > 0.0f)
+            {
+                m_world.rAmbient -= 0.1f;
+
+            }
+        }
+        private void gMinus(object sender, RoutedEventArgs e)
+        {
+
+            if (m_world.gAmbient > 0.0f)
+            {
+                m_world.gAmbient -= 0.1f;
+            }
+        }
+        private void bMinus(object sender, RoutedEventArgs e)
+        {
+            if (m_world.bAmbient > 0.0f)
+            {
+                m_world.bAmbient -= 0.1f;
+            }
+        }
+        private void rPlus(object sender, RoutedEventArgs e)
+        {
+            if (m_world.rAmbient < 1.0f)
+            {
+                m_world.rAmbient += 0.1f;
+            }
+        }
+        private void gPlus(object sender, RoutedEventArgs e)
+        {
+            if (m_world.gAmbient < 1.0f)
+            {
+                m_world.gAmbient += 0.1f;
+            }
+        }
+        private void bPlus(object sender, RoutedEventArgs e)
+        {
+            if (m_world.bAmbient < 1.0f)
+            {
+                m_world.bAmbient += 0.1f;
+            }
+        }
+
+
     }
 }
+
--- AssimpSample/World.cs
+++ AssimpSample/World.cs
@@ -14,6 +14,9 @@ using SharpGL.SceneGraph.Primitives;
 using SharpGL.SceneGraph.Quadrics;
 using SharpGL.SceneGraph.Core;
 using SharpGL;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Threading;
 
 namespace AssimpSample
 {
@@ -54,7 +57,7 @@ namespace AssimpSample
         /// <summary>
         ///	 Udaljenost scene od kamere.
         /// </summary>
-        private float m_sceneDistance = 700.0f;
+        private float m_sceneDistance = 500.0f;
 
         /// <summary>
         ///	 Sirina OpenGL kontrole u pikselima.
@@ -66,6 +69,49 @@ namespace AssimpSample
         ///	 Visina OpenGL kontrole u pikselima.
         /// </summary>
         private int m_height;
+
+
+        /// <summary>
+        ///	 Scaling factor for model
+        /// </summary>
+        private float mm_scale = 1.0f;
+
+        private enum TextureObjects { PODLOGA = 0, STEPENICE, OSOBA };
+        private readonly int m_textureCount = Enum.GetNames(typeof(TextureObjects)).Length;
+        private uint[] m_textures = null;
+        private string[] m_textureFiles = { "..//..//Textures//plocice.jpg", "..//..//Textures//metal.jpg", "..//..//Textures//osoba.jpg" };
+
+        private float[] m_ambientColor = { 0.2f, 0.2f, 0.2f, 0.2f };
+
+        private float scaleUlaz = 140.0f;
+        private float rotateY = 0.0f;
+
+        private float plus = 0.0f;
+        public float rAmbient = 0.3f;
+        public float gAmbient = 0.3f;
+        public float bAmbient = 0.3f;
+
+
+
+        public float Plus { get => plus; set => plus = value; }
+
+        public float ScaleUlaz { get => scaleUlaz; set => scaleUlaz = value; }
+        public float RotateY { get => rotateY; set => rotateY = value; }
+
+        public static bool startAnimation = false;
+        public static DispatcherTimer timer;
+        public static float x = -180f;
+        public static float y = -205f;
+        public static float z = -100f;
+        public static float rot_y = 0f;
+        public static bool endAnimation = false;
+        public static bool disapper = false;
+
+        /// <summary>
+        /// Pomeranje brzine pingvina
+        /// </summary>
+        public static float brzinaPingvina = 10.0f;
+        public static float skokPingvina = 200.0f;
 
         #endregion Atributi
 
@@ -124,6 +170,11 @@ namespace AssimpSample
             get { return m_height; }
             set { m_height = value; }
         }
+        public float MMScale
+        {
+            get { return mm_scale; }
+            set { mm_scale = value; }
+        }
 
         #endregion Properties
 
@@ -137,6 +188,8 @@ namespace AssimpSample
             this.m_scene = new AssimpScene(scenePath, sceneFileName, gl);
             this.m_width = width;
             this.m_height = height;
+
+            m_textures = new uint[m_textureCount];
         }
 
         /// <summary>
@@ -158,15 +211,123 @@ namespace AssimpSample
         {
             gl.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
             gl.Color(1f, 0f, 0f);
+
+
+
+            gl.Enable(OpenGL.GL_COLOR_MATERIAL);
+            gl.ColorMaterial(OpenGL.GL_FRONT, OpenGL.GL_AMBIENT_AND_DIFFUSE);
+            //gl.Enable(OpenGL.GL_TEXTURE_2D);
+            // gl.Enable(OpenGL.GL_LIGHTING);
+
+            SetupRedLight(gl);
+            SetupRedLight(gl);
+
+            gl.Enable(OpenGL.GL_NORMALIZE);
+
             m_scene.LoadScene();
             m_scene.Initialize();
+            LoadTextures(gl);
+            SetupLighting(gl);
+        }
+
+        public void SetupLighting(OpenGL gl)
+        {
+
+            //color tracking - definisanje materijala
+            //ambijentalna - sveprisutna svetlost iz svih pravaca, kao dnevna
+            //difuzna - osvetljava objekte iz nekog pravca, refleksija pod normalnim uglom, stona lampa
+           
+
+
+            gl.Enable(OpenGL.GL_LIGHTING); //OMOGUCI SVETLO
+            gl.Enable(OpenGL.GL_LIGHT0); //UKLJUCI SVETLO PORED STEPENICA
+            //gl.Enable(OpenGL.GL_LIGHT1);// UKLJUCI SVETLO NA ANTENI
+
+           // float[] RED = new float[] { 1.0f, 0f, 0f }; //CRVENA BOJA
+                                                        //float[] WHITE = new float[] { 1.0f,1.0f,1f }; //BELA BOJA
+                                                        //tackasti izvor, stacionaran
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_SPOT_CUTOFF, 180.0f); //TACKASTI IZVOR
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_AMBIENT, new float[] { 1f, 1f, 1f });//AKO IH STAVIM SVE NA 1, ONDA BUDE PREVISE BELA CELA SCENA
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_DIFFUSE, new float[] { 0.7f, 0.7f, 0.7f });
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_SPECULAR, new float[] { 0.3f, 0.3f, 0.3f });
+
+            float[] pos = { 3500, 500, 0f, 1.0f };
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_POSITION, pos);
+            //POZICIJU ZA STACIONARNO DEFINISEM OVDE KAKO KASNIJE NIJEDNA TRANSFORMACIJA NE BI UTICALA NA NJEGA
+
+
+            //iznad hangara, cut off 25
+            //gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_SPOT_CUTOFF, 25f);//REFLEKTORSKI         
+            //gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_DIFFUSE, RED);
+            //gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_SPECULAR, RED);
+            //POZICIJU ZA OVO SVETLO DEFINISEM POSLE, KOD CRTANJA ANTENE, JER TREBA UVEK DA BUDE NA VRHU ANTENE
+    
+
+        }
+
+        private void SetupRedLight(OpenGL gl)
+        {
+            float[] light1pos = new float[] { -250.0f, 500.0f, -500.0f, 1.0f };
+            float[] light1diffuse = new float[] { 1f, 0f, 0f, 1.0f };
+            float[] light1ambient = new float[] { 0.2f, 0f, 0f, 1.0f };
+            float[] light1direction = new float[] { 0.0f, -1.0f, 0.0f, 1.0f };
+
+            //  gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_AMBIENT, light1ambient);
+            gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_SPECULAR, light1diffuse);
+            gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_DIFFUSE, light1diffuse);
+            gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_SPOT_DIRECTION, light1direction);
+            gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_SPOT_CUTOFF, 40.0f);
+            gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_POSITION, light1pos);
+
+            gl.Enable(OpenGL.GL_LIGHT1);
+            gl.Enable(OpenGL.GL_LIGHTING);
+
+
+        }
+
+        private void LoadTextures(OpenGL gl)
+        {
+            //nacin stapanja teksture sa materijalom GL_ADD
+            gl.TexEnv(OpenGL.GL_TEXTURE_ENV, OpenGL.GL_TEXTURE_ENV_MODE, OpenGL.GL_ADD);
+            gl.Enable(OpenGL.GL_TEXTURE_2D);
+
+            gl.GenTextures(m_textureCount, m_textures);
+
+            for (int i = 0; i < m_textureCount; ++i)
+            {
+
+                gl.BindTexture(OpenGL.GL_TEXTURE_2D, m_textures[i]);
+
+                Bitmap image = new Bitmap(m_textureFiles[i]);
+                image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+                BitmapData imageData = image.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                //kreiraj teksturu
+                gl.TexImage2D(OpenGL.GL_TEXTURE_2D, 0, (int)OpenGL.GL_RGBA8, imageData.Width, imageData.Height, 0, OpenGL.GL_BGRA, OpenGL.GL_UNSIGNED_BYTE, imageData.Scan0);
+
+
+                //gl REPEAT po obema osama
+                gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_WRAP_S, OpenGL.GL_REPEAT);
+                gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_WRAP_T, OpenGL.GL_REPEAT);
+
+                //najblizi sused filtriranje
+                gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_MIN_FILTER, OpenGL.GL_NEAREST);
+                gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_MAG_FILTER, OpenGL.GL_NEAREST);
+
+
+
+                image.UnlockBits(imageData);
+                image.Dispose();
+
+
+            }
         }
 
 
-        /// <summary>
-        /// Podesava viewport i projekciju za OpenGL kontrolu.
-        /// </summary>
-        public void Resize(OpenGL gl, int width, int height)
+            /// <summary>
+            /// Podesava viewport i projekciju za OpenGL kontrolu.
+            /// </summary>
+            public void Resize(OpenGL gl, int width, int height)
         {
             m_width = width;
             m_height = height;
@@ -189,6 +350,13 @@ namespace AssimpSample
             gl.Enable(OpenGL.GL_CULL_FACE);
             gl.Enable(OpenGL.GL_DEPTH_TEST);
 
+            gl.LoadIdentity();
+
+            gl.LookAt(0f, 200.0f, m_sceneDistance, 0, 0, 0, 0.0f, 1.0f, 0.0f);
+            float[] light0ambient = new float[] { rAmbient, gAmbient, bAmbient, 1.0f };
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_AMBIENT, light0ambient);
+
+
             gl.PushMatrix();
             {
 
@@ -200,9 +368,16 @@ namespace AssimpSample
                 gl.Rotate(m_xRotation, 1.0f, 0.0f, 0.0f);
                 gl.Rotate(m_yRotation, 0.0f, 1.0f, 0.0f);
 
+                gl.PushMatrix();
+
+                gl.Scale(1, mm_scale, 1);
+               
+
                 // person
                 gl.PushMatrix();
                 DrawModel(gl);
+                gl.PopMatrix();
+
                 gl.PopMatrix();
 
                 // podloga
@@ -253,9 +428,19 @@ namespace AssimpSample
         {
 
             gl.PushMatrix();
-            gl.Translate(-180, -205, -100f);
-            gl.Scale(2.5f, 2.5f, 2.5f);  
+            gl.TexEnv(OpenGL.GL_TEXTURE_ENV, OpenGL.GL_TEXTURE_ENV_MODE, OpenGL.GL_MODULATE);
+
+            
+            gl.Translate(x, y, z);
+            gl.Scale(2.5f, 2.5f, 2.5f);
+            gl.Scale(1f, mm_scale, 1f);
             gl.Rotate(0.0f, 135f, 0);
+
+
+            //gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_SPOT_DIRECTION, new float[] { 0.0f, -1.0f, 0.0f });
+            //gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_POSITION, new float[] { 5.0f, 50.0f, 0f, 1.0f });
+
+            //gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_AMBIENT, m_ambientColor);
 
             m_scene.Draw();
 
@@ -270,11 +455,13 @@ namespace AssimpSample
             gl.Rotate(0.0f, -35f, -0.0f);
             gl.Translate(-250, -274f, -40f);
             gl.Scale(150f, 20f, 30f);
-         
-       
-            gl.Color(1f, 0.5f, 1f);
+            //gl.Color(1f, 0.5f, 1f);
+
+            gl.BindTexture(OpenGL.GL_TEXTURE_2D, m_textures[(int)TextureObjects.STEPENICE]);
+            
             Cube stepenica = new Cube();
             stepenica.Render(gl, SharpGL.SceneGraph.Core.RenderMode.Render);
+
 
             //druga stepenica
             gl.Translate(0, 2f, -2f);
@@ -310,23 +497,43 @@ namespace AssimpSample
         {
             float[] podloga =
             {
-                1000.0f, -300.0f,  170.0f,
-                1000.0f, -300.0f, -1000.0f,
-                -1000.0f, -300.0f, -1000.0F,
-                -1000.0f, -300.0f,  170.0f,
-             
-              
-                 
+               2000.0f, -300.0f,  370.0f,
+                2000.0f, -300.0f, -1800.0f,
+                -500.0f, -300.0f, -1800.0F,
+                -500.0f, -300.0f,  370.0f,
+
+
             };
 
-            gl.Color(1.0f, 1.0f, 1.0f);
+            float[] textCoords =
+            {
+                1.0f, 0.0f,
+                1.0f, 1.0f,
+                0.0f, 1.0f,
+                0.0f, 0.0f
+            };
+
+           // gl.TexEnv(OpenGL.GL_TEXTURE_ENV, OpenGL.GL_TEXTURE_ENV_MODE, OpenGL.GL_ADD);
+
+            gl.BindTexture(OpenGL.GL_TEXTURE_2D, m_textures[(int)TextureObjects.PODLOGA]);
+            //gl.TexEnv(OpenGL.GL_TEXTURE_ENV, OpenGL.GL_TEXTURE_ENV_MODE, OpenGL.GL_ADD);
+            gl.MatrixMode(OpenGL.GL_TEXTURE);
+            gl.LoadIdentity();
+            gl.Scale(1f, 1f, 1f);
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+          
+            gl.Color(0.5f, 0.5f, 0.5f);
             gl.Begin(OpenGL.GL_QUADS);
+            gl.Normal(0, 1, 0);
             int i = 0;
+            int j = 0;
             while (i < podloga.Length)
             {
+                gl.TexCoord(textCoords[j], textCoords[j + 1]);
                 gl.Vertex(podloga[i], podloga[i + 1], podloga[i + 2]);
 
                 i += 3;
+                j += 2;
             }
 
             gl.End();
@@ -344,6 +551,83 @@ namespace AssimpSample
             gl.DrawText(160, 25, 1.0f, 0.0f, 0.0f, "Verdana Italic", 10, "Sifra zad: 11.1");
 
         }
+
+        public static void Animacija(object sender, EventArgs e)
+        {
+
+            if (y == -205.0f)
+            {
+                timer.Start();
+                y += 45f;
+                z += -50f;
+                x += 50f;
+
+            }
+            else if (y == -160.0f)
+            {
+                y += 42f;
+                z += -47f;
+                x += 50f;
+            }
+            else if (y == -118f)
+            {
+                y += 42f;
+                z += -40f;
+                x += 50f;
+            }
+            else if (y == -76f)
+            {
+                y += 40f;
+                z += -30f;
+                x += 50f;
+            }
+            else if (y == -36f)
+            {
+                y += 38f;
+                z += -35f;
+                x += 50f;
+            }
+            else if (y == 2f)
+            {
+                y += 38f;
+                z += -35f;
+                x += 50f;
+            }
+            else if (y == 40f)
+            {
+                y += 38f;
+                z += -35f;
+                x += 50f;
+            }
+            /*else if (x == -150f)
+            {
+                y -= 20f;
+                x += 10f;
+                rot_y = 10f;
+            }
+            else if (x == -160)
+            {
+                x += 10f;
+                rot_y *= -1;
+            }
+            else if (x > -160f && x < 0f)
+            {
+                x += 10f;
+                rot_y *= -1;
+            } */
+            else if (y == 130)
+            {
+                startAnimation = false;
+                endAnimation = true;
+
+               // x = 0f;
+                //y = -100f;
+                //rot_y = 0f;
+
+                timer.Stop();
+            }
+        }
+
 
         #endregion Metode
 
